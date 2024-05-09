97f7dfaa5d4884af423a62ba573d696b99a88f33
nolotbublika <161640172+nolotbublika@users.noreply.github.com>
2024-05-09 14:22:47 +0300

    Add files via upload

---
+++ мапы и фильтры.py
@@ -0,0 +1,19 @@
+def is_prime(func):
+    def wrapper(a, b, c):
+        result = func(a, b, c)
+        if result < 2:
+            return "Составное"
+        for i in range(2, int(result ** 0.5) + 1):
+            if result % i == 0:
+                return "Составное"
+        return "Простое"
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
+
+
